70b7143c10004adcd4138b7954fe5be261c7f34c
jamhyunsang <gussang95@gmail.com>
2020-10-15 19:13:41 +0900

    NGUI 마호마호 완성
    1. 2D리소스 -> Atlas 만들기
    2. NGUI Font 만들기
    3. 위젯 생성 및 배치
    4. C# Scripts 만들기 (Player, GM)
    5. NGUI 창 맞추기

---
+++ NGUI/Assets/02. Scripts/GM.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GM : MonoBehaviour
+{
+    public GameObject bgSet;
+    public GameObject bgSetDam;
+    public float speed;
+    public Vector3 bgPos;
+    public Vector3 bgPosDam;
+    public UILabel scoreLabel;
+    public GameObject enemySet;
+    public List<GameObject> childEnemy = new List<GameObject> { };
+    public float xMove1;
+    public float xMove2;
+    public GameObject gameOver;
+    public UILabel gameOverScore;
+
+    int enemyIdx = 0;
+    float yPos;
+    float scoreF = 0.0f;
+    int score;
+    float xPosChk = 0.0f;
+    float xPosChkDam = 0.0f;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bgPos = bgSet.transform.position;
+        bgPosDam = bgSetDam.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        xPosChk += speed;
+        xPosChkDam += speed;
+        bgSet.transform.Translate(Vector3.left * speed * Time.deltaTime * 0.5f);
+        bgSetDam.transform.Translate(Vector3.left * speed * Time.deltaTime);
+        enemySet.transform.Translate(Vector3.left * speed * Time.deltaTime);
+
+        if (xPosChk > 960.0f)
+        {
+            xPosChk = 0.0f;
+            bgSet.transform.position = bgPos;
+        }
+
+        if (xPosChkDam > 960.0f)
+        {
+            xPosChkDam = 0.0f;
+            bgSetDam.transform.position = bgPosDam;
+        }
+        if (enemySet.transform.position.x < 0.0f)
+        {
+            Debug.Log(enemySet.transform.localPosition.x);
+        }
+
+            if (enemySet.transform.localPosition.x < xMove1)
+        {
+            xMove1 -= xMove2;
+            ReSetChildSet();
+            enemyIdx++;
+            if(enemyIdx>=childEnemy.Count)
+            {
+                enemyIdx = 0;
+            }
+            Debug.Log("Move");
+        }
+
+        scoreF += speed * Time.deltaTime;
+        score = (int)scoreF;
+        scoreLabel.text = score.ToString();
+    }
+
+    private void ReSetChildSet()
+    {
+        Debug.Log("Reset");
+        childEnemy[enemyIdx].transform.localPosition += new Vector3(1440.0f, 0, 0);
+        switch(Random.Range(1,4))
+        {
+            case 1:
+                yPos = -140.0f;
+                break;
+            case 2:
+                yPos = 0f;
+                break;
+            case 3:
+                yPos = 140.0f;
+                break;
+        }
+        childEnemy[enemyIdx].transform.localPosition = new Vector3(childEnemy[enemyIdx].transform.localPosition.x, yPos, childEnemy[enemyIdx].transform.localPosition.z);
+    }
+
+    public void GameOver()
+    {
+        Time.timeScale = 0;
+        gameOver.SetActive(true);
+        gameOverScore.text = score.ToString();
+    }
+
+    public void ReStart()
+    {
+        gameOver.SetActive(false);
+        Time.timeScale = 1;
+        Application.LoadLevel(0);
+    }
+}
---
+++ NGUI/Assets/02. Scripts/PlayerMove.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed;
+    public UISprite hpSprite;
+    public GM gm;
+
+    float maxHp = 100.0f;
+    float hp = 100.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        float V = Input.GetAxis("Vertical");
+        Vector3 dir = new Vector3(0, V, 0);
+        transform.Translate(dir * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        hp -= 10.0f;
+        hpSprite.fillAmount = hp * 0.01f;
+
+        if(hp <= 0)
+        {
+            gm.GameOver();
+        }
+    }
+
+}
